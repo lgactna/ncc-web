ae8eb7d8a9cace501abc7d05d05695318990b8e0
lgactna <lgonzalesna@gmail.com>
2022-12-11 18:28:01 -0800

    add minimal authentication system

---
+++ README.md
@@ -0,0 +1,6 @@
+Settings for BSS export:
+- Minify JS, CSS
+- Use CDN for libraries
+- Export labels
+
+Export to /bss_export.
--- src/ncc_web/urls.py
+++ src/ncc_web/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("writeups/", include("writeups.urls")),
     path("tinymce/", include("tinymce.urls")),
+    path("accounts/", include('django.contrib.auth.urls')),
 ]
 
 # Redirect root to writeups/ (for now)
