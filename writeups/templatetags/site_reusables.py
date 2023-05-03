77fbd88fe495db6f27ea3ebff8dc87009bd06c47
Lloyd Gonzales <lgonzalesna@nevada.unr.edu>
2023-05-02 21:56:53 -0700

    Close #10, close #13, close #14

--- writeups/templatetags/site_reusables.py
+++ writeups/templatetags/site_reusables.py
@@ -21,6 +21,27 @@ def show_recent_posts(context, is_authenticated):
 
     return {'recent_posts': recent_posts}
 
+@register.inclusion_tag('all_posts.html', takes_context=True)
+def show_all_posts(context, is_authenticated):
+    """
+    Inclusion tag functionality associated with the columnar "recent posts"
+    listing on various pages. This expects that `posts` has been included
+    inside the context, which is a QuerySet of Posts.
+
+    Note that this automatically uses is_authenticated to determine if
+    private posts should be filtered. It's the exact same as show_recent_posts,
+    except all the posts are returned, and a table is used instead.
+    """
+
+    if not is_authenticated:
+        all_posts = context['posts'].filter(private=False)
+    else:
+        all_posts = context['posts'].all()
+    
+    all_posts = all_posts.order_by("-post_time")[:5]
+
+    return {'all_posts': all_posts}
+
 @register.inclusion_tag('competition_cards.html')
 def get_competition_cards(competitions):
     """
